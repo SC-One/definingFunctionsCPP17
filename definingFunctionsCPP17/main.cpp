cab81177737c76e1b5d08f5d753eeb36ae4e270e
heydar <heydar.mahmoodi75@gmail.com>
2020-03-21 06:18:56 -0400

    First example for defeining a function

---
+++ definingFunctionsCPP17/main.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+
+
+/*
+
+    return_type function_name(parameter1 , parameter2 , parameter3 , ...)
+    {
+        // code of function's job
+    }
+
+    //return_type can be any type Or nothing!
+    //function_name follow variable's rule.
+    //parameter_list can be anything that you send it to function.
+*/
+
+
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ first function
+void printHello(void)
+{
+    std::cout<<"Hello"<<std::endl;
+}
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+
+int main()
+{
+    //first example
+    printHello();
+    return 0;
+}
