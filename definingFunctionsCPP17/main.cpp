15c97c2dc4d75635890d25b00f20d2650356afe4
heydar <heydar.mahmoodi75@gmail.com>
2020-03-22 17:44:35 -0400

    passing multidemontion array to functions by pointer as const

--- definingFunctionsCPP17/main.cpp
+++ definingFunctionsCPP17/main.cpp
@@ -1,20 +1,26 @@
 #include<iostream>
-double averageNumbers(const double array[],const size_t lengthNumbers);
+#define secondDemonsionSizeOfMarks 5
+double sumMarks(const double numbers[][secondDemonsionSizeOfMarks],const size_t *sizeMarks);
 int main()
 {
-    double marks[] {1,2,3,4,5,6,7,8,9,10};
-    size_t size=sizeof (marks)/sizeof (marks[0]);
-    std::cout<<averageNumbers(marks,size)<<std::endl;
+    double marks[][secondDemonsionSizeOfMarks] {
+                                                 {1,2,3,4,5}
+                                                ,{6,7,8,9,10}
+                                                ,{11,12,13,14,15}
+                                               };
+    size_t row =  sizeof marks / sizeof marks[0];
+    size_t col = sizeof marks[0] / sizeof(marks[0][0]);
+    size_t size{row*col};
+    std::cout<<sumMarks(marks,&size)<<std::endl;
     return 0;
 }
 
-double averageNumbers(const double array[],const size_t lengthNumbers)
+
+double sumMarks(const double numbers[][secondDemonsionSizeOfMarks],const size_t *sizeMarks)
 {
     double sum{};
-    for (size_t i=0;i<lengthNumbers;i++)
-    {
-        sum+=array[i];
-    }
-    sum/=lengthNumbers;
+    for(size_t i=0;i<((*sizeMarks)/(secondDemonsionSizeOfMarks));i++)
+        for(size_t j=0;j<secondDemonsionSizeOfMarks;j++)
+            sum+=numbers[i][j];
     return sum;
 }
