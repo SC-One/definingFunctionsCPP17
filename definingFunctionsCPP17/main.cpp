a4b321ceda34f5140626f82db6eee2a8e99753a6
heydar <heydar.mahmoodi75@gmail.com>
2020-03-22 07:44:37 -0400

    Prototype Functions definition in source file

--- definingFunctionsCPP17/main.cpp
+++ definingFunctionsCPP17/main.cpp
@@ -1,5 +1,22 @@
 #include<iostream>
 
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@ Prototype functions @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+double powerMath(double value,int exponent); // function prototype
+// does not matter what's these names :: Value and Exponent
+
+
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@  Main function      @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+int main()
+{
+    std::cout<<powerMath(2,2)<<std::endl;
+    std::cout<<powerMath(2,-3)<<std::endl;
+    std::cout<<powerMath(-2,2)<<std::endl;
+    std::cout<<powerMath(-2,3)<<std::endl;
+    std::cout<<powerMath(-1000,0)<<std::endl;
+    return 0;
+}
+
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@ function's Header @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 double powerMath(double x,int n)
 {
     double result{1};
@@ -15,14 +32,4 @@ double powerMath(double x,int n)
     for(int i=0;i<n;i++)
         result*=x;
     return result;
-}
-
-int main()
-{
-    std::cout<<powerMath(2,2)<<std::endl;
-    std::cout<<powerMath(2,-3)<<std::endl;
-    std::cout<<powerMath(-2,2)<<std::endl;
-    std::cout<<powerMath(-2,3)<<std::endl;
-    std::cout<<powerMath(-1000,0)<<std::endl;
-    return 0;
 }
