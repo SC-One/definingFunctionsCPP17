8fa7c3883cfd59632a24193f63c433f434e6be37
heydar <heydar.mahmoodi75@gmail.com>
2020-03-23 04:18:16 -0400

    Passing parameter to function by refrence

--- definingFunctionsCPP17/main.cpp
+++ definingFunctionsCPP17/main.cpp
@@ -1,24 +1,15 @@
 #include<iostream>
-#define secondDemonsionSizeOfMarks 5
-double sumMarks(const double numbers[][secondDemonsionSizeOfMarks],const size_t *row);
+void changeNumber(double &value);
 int main()
 {
-    double marks[][secondDemonsionSizeOfMarks] {
-                                                 {1,2,3,4,5}
-                                                ,{6,7,8,9,10}
-                                                ,{11,12,13,14,15}
-                                               };
-    size_t row =  sizeof marks / sizeof marks[0];
-    std::cout<<sumMarks(marks,&row)<<std::endl;
+    double value{100};
+    std::cout<<"Before passing to function by refrence: "<<value<<std::endl;
+    changeNumber(value);   // just need its name , not address!
+    std::cout<<"After passing to function by refrence: "<<value<<std::endl;
     return 0;
 }
-
-
-double sumMarks(const double numbers[][secondDemonsionSizeOfMarks],const size_t *row)
-{
-    double sum{};
-    for(size_t i=0;i<(*row);i++)
-        for(auto& val:numbers[i])
-            sum+=val;
-    return sum;
+void changeNumber(double &value) // same to passing by pointer !
+{                                // but it have one advantage!
+    value+=20;                   // we dont need pointer notation and when use it in main
+    return;                      // dont need send its address! just name Of variable need.
 }
