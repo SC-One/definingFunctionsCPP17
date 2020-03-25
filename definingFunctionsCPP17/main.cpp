420b113262768aa398e4c8ba4454210b1bbda1cb
heydar <heydar.mahmoodi75@gmail.com>
2020-03-25 05:31:15 -0400

    default arguments for int main() , run it in terminal by following hint in file

--- definingFunctionsCPP17/main.cpp
+++ definingFunctionsCPP17/main.cpp
@@ -1,60 +1,12 @@
 #include<iostream>
-#include<vector>
-#include<string>
-#include<iomanip>
-void findWordsInText(std::string_view text,std::string_view seprators,std::vector<std::string> &words);
-size_t findMaxSizeWords(const std::vector<std::string> &words);
-void printListWords(const std::vector<std::string> &words,const size_t &max);
-int main()
+int main(int argc,char *argv[])
 {
-    std::string text;
-    std::cout<<"Insert text that terminated by '*': ";
-    std::getline(std::cin,text,'*');
+    for (int i=0;i<argc;i++)
+    {
+        std::cout<<argv[i]<<std::endl;
+    }
+    // you can run in linux:
+    // ./nameOfProgram.o argc1 argc2 argc3 ...
     std::cout<<std::endl;
-    std::vector<std::string> words;
-    const std::string separators{" ,;:.\"!?'\n"};
-    findWordsInText(text,separators,words);
-    printListWords(words,findMaxSizeWords(words));
     return 0;
-}
-void findWordsInText(std::string_view text,std::string_view seprators,std::vector<std::string> &words)
-{
-    size_t start{text.find_first_not_of(seprators)};
-    size_t end{};
-    while(start!=std::string::npos)
-    {
-        end = text.find_first_of(seprators,start+1);
-        if(end==std::string::npos)
-        {
-            end=text.size();
-        }
-        words.push_back(std::string{text.substr(start,end-start)});
-        start=text.find_first_not_of(seprators,end+1);
-    }
-    return;
-}
-
-
-size_t findMaxSizeWords(const std::vector<std::string> &words)
-{
-    size_t max{};
-    for(const auto &x:words)
-    {
-        if(x.length()>max)
-            max=x.length();
-    }
-    return max;
-}
-
-void printListWords(const std::vector<std::string> &words,const size_t &max)
-{
-    size_t count{};
-    for(const auto &x:words)
-    {
-        std::cout<<std::left<<std::setw(max+3)<<x;
-        if(++count%5==0)
-            std::cout<<std::endl;
-    }
-    std::cout<<std::endl;
-    return;
 }
